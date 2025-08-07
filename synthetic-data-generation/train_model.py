ae449a31fe61b50dbc183d13f60f4d807327d71b
Sebastian Joerz <sjoerz@skyface.de>
2025-08-07 14:05:44 +0200

    cleanup

--- synthetic-data-generation/train_model.py
+++ synthetic-data-generation/train_model.py
@@ -6,15 +6,18 @@ import os
 def run(dataset_dir, num_epochs, results_dir):
 
     model = YOLO("yolo12m.pt")
-    train_res = model.train(data=f"{dataset_dir}/data.yaml", epochs=num_epochs, batch=0.8, save=True, seed=42, augment=True)
+    train_res = model.train(data=f"{dataset_dir}/data.yaml",
+                            epochs=num_epochs, batch=0.8, save=True, seed=42, augment=True)
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
     save_dir = model.trainer.save_dir
     print("Result file:", str(save_dir) + "/results.png")
-    subprocess.run(f'cp {str(save_dir)}/results.png {results_dir}/results.png', shell=True)
-    subprocess.run(f'cp {str(save_dir)}/results.txt {results_dir}/results.txt', shell=True)
-    subprocess.run(f'cp {save_dir}/weights/best.pt {results_dir}/best.pt', shell=True)
-
+    subprocess.run(
+        f'cp {str(save_dir)}/results.png {results_dir}/results.png', shell=True)
+    subprocess.run(
+        f'cp {str(save_dir)}/results.txt {results_dir}/results.txt', shell=True)
+    subprocess.run(
+        f'cp {save_dir}/weights/best.pt {results_dir}/best.pt', shell=True)
 
     test_res = model.val(data=f"{dataset_dir}/data.yaml", split="test")
     results_dict = test_res.results_dict
@@ -27,8 +30,10 @@ def run(dataset_dir, num_epochs, results_dir):
     with open(f"{results_dir}/results.txt", "w") as f:
         f.write(f"MAP50: {mAP50}\n")
         f.write(f"MAP50-95: {mAP50_95}\n")
-        
-    print(f"Now you should copy the {results_dir}/best.pt to Object_Detection_Yolo_with_FastAPI/model.pt")
+
+    print(
+        f"Now you should copy the {results_dir}/best.pt to Object_Detection_Yolo_with_FastAPI/model.pt")
+
 
 if __name__ == '__main__':
     import argparse
@@ -42,6 +47,5 @@ if __name__ == '__main__':
     parser.add_argument('--results_dir', type=str,
                         help='Path to the results directory')
     args = parser.parse_args()
-    
+
     run(args.dataset_dir, args.num_epochs, args.results_dir)
-    
